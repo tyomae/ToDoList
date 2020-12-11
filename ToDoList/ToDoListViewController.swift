d42af41ef3f7f2032434ccbeba4b897f51330e44
Artem <dom1nuk@yandex.ru>
2020-12-11 16:38:52 +0300

    Project created

---
+++ ToDoList/ToDoListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Артем  Емельянов  on 11.12.2020.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+}
+
